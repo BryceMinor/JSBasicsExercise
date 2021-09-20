d915677d5cc7ee0c013a544b0a3d5cb99ff62384
Bryce Minor <bryce.minor7@gmail.com>
2021-09-20 14:12:45 -0500

    Completed initial version of JS Basics exercise

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log(7);
+
+// Numbers, strings, booleans, null, undefined
+
+// +, -, *, /, **, %
+
+console.log(1%5);
+console.log(6%5);
+
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+console.log("Hawk" + "eye");
+
+console.log("Bryce".length + "Minor".length);
+
+console.log("Avengers: Endgame"[16]);
